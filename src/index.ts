5eb581594c6cebde4a4ebb76206ec66f5d1da9cd
Albano <Albano Xhafaj>
2024-09-18 14:03:16 +0200

    feature: added initial middlewares

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-console.log('Hello World');
+import path from 'node:path';
+
+import express from 'express';
+import helmet from 'helmet';
+import cors from 'cors';
+
+const PORT = 3000;
+const URL = `http://localhost:${PORT}`;
+
+const app = express();
+
+app.use(
+  helmet(),
+  cors({ origin: URL }),
+  express.static(path.resolve(__dirname, '../public'))
+);
+
+app.listen(PORT, () => {
+  console.log(`Listening on: ${URL}`);
+});
